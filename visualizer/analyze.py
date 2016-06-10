adb8eb09ca574df44a9bfaa5f6a58e6e3a18aac3
Dana Fallon <danajfallon@gmail.com>
2016-06-10 15:13:10 -0700

    Add amplitude wave

--- visualizer/analyze.py
+++ visualizer/analyze.py
@@ -2,19 +2,26 @@ import mock
 import sys
 sys.modules.update((mod_name, mock.Mock()) for mod_name in ['matplotlib', 'matplotlib.pyplot', 'matplotlib.image'])
 
+import numpy as np
 import librosa
-
-
-EX_FILEPATH = 'songs/imitosis.wav'
 
 
 def get_song_data(filepath):
     y, sr = librosa.load(filepath)
     song_length_ms = len(y) / float(sr) * 1000
-    chromagram = librosa.feature.chroma_stft(y=y, sr=sr).T 	# transpose
+    S = np.abs(librosa.stft(y)).T
+    rmse = librosa.feature.rmse(y=y)
+    logamp = librosa.logamplitude(S ** 2)
+    amplitudes = np.array([y[i] for i in np.arange(0, len(y), 512)])
+    y_harmonic, y_percussive = librosa.effects.hpss(y)
+    chromagram = librosa.feature.chroma_stft(y=y_harmonic, sr=sr).T
     chroma_frame_length_ms = song_length_ms / float(chromagram.shape[0])
     return {
         'song_length_ms': song_length_ms,
+        'spectrogram': S,
+        'rmse': rmse[0],
+        'logamp': logamp,
+        'amplitudes': amplitudes,
         'chromagram': chromagram,
         'chroma_frame_length_ms': chroma_frame_length_ms
     }
--- visualizer/animate.py
+++ visualizer/animate.py
@@ -1,10 +1,16 @@
+from itertools import cycle
+import sys
+
+import numpy as np
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import pygame
 from pygame.locals import *
 
-from analyze import get_song_data, EX_FILEPATH
+from analyze import get_song_data
 
+
+EX_FILEPATH = 'songs/imitosis.wav'
 
 colors = (
     (1, 0, 0),
@@ -24,9 +30,9 @@ colors = (
 
 def plot_chromagram(chroma_frame):
     # chroma_frame is a 1d numpy array with length 12
+    x_vals = [i * (1 / 6.0) - 1 for i in range(12)]
     glLineWidth(10.0)
     glBegin(GL_LINES)
-    x_vals = [i * (1 / 6.0) - 1 for i in range(12)]
     for x, y, color in zip(x_vals, chroma_frame, colors):
         glColor(color)
         glVertex(x, 0, 0)
@@ -34,9 +40,21 @@ def plot_chromagram(chroma_frame):
     glEnd()
 
 
-def animate():
-    song_data = get_song_data(EX_FILEPATH)
+def draw_amp_wave(amp):
+    x_abs = np.pi * 6
+    x_vals = np.linspace(-x_abs, x_abs, 200)
+    glLineWidth(1.0)
+    glColor((1, 1, 1))
+    glBegin(GL_LINE_STRIP)
+    for x, y in zip(x_vals, np.sin(x_vals)):
+        glVertex(x / x_abs, y * amp * 10 / x_abs - 0.5, 0)
+    glEnd()
+
+
+def animate(song_filepath):
+    song_data = get_song_data(song_filepath)
     chromagram = song_data['chromagram']
+    amplitudes = song_data['amplitudes']
     chroma_frame_length_ms = song_data['chroma_frame_length_ms']
 
     pygame.init()
@@ -44,12 +62,14 @@ def animate():
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-    glTranslatef(0.1, -0.5, -3)
+    glTranslatef(0.1, 0.0, -3)
 
     pygame.mixer.init()
-    pygame.mixer.music.load(EX_FILEPATH)
+    pygame.mixer.music.load(song_filepath)
     pygame.mixer.music.play()
-    for frame_i in range(chromagram.shape[0]):
+    pygame.time.delay(int(chroma_frame_length_ms))
+    num_frames = chromagram.shape[0]
+    for frame_i in range(num_frames):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -59,9 +79,16 @@ def animate():
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         plot_chromagram(chromagram[frame_i])
+        draw_amp_wave(amplitudes[frame_i])
 
         pygame.display.flip()
         pygame.time.delay(int(chroma_frame_length_ms))
 
 
-animate()
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) > 1:
+        filepath = args[1]
+    else:
+        filepath = EX_FILEPATH
+    animate(filepath)
